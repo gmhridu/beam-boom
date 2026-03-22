979da113bd985ef77eeaabce5a57544460b7c39b
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2026-03-22 23:15:38 +0600

    chore: Comment out the right image component in the pigmentation and oily skin about sections.

--- src/app/skin-condition/oily-skin/_components/about.tsx
+++ src/app/skin-condition/oily-skin/_components/about.tsx
@@ -91,7 +91,7 @@ export default function About() {
           </div>
 
           {/* Right Image */}
-          <div className="relative order-1 lg:order-2 flex justify-center lg:justify-end">
+          {/* <div className="relative order-1 lg:order-2 flex justify-center lg:justify-end">
             <div className="relative h-70 sm:h-87.5 md:h-100 lg:h-125 w-full sm:w-87.5 md:w-112.5 lg:w-150 overflow-hidden z-10">
               <Image
                 src="https://res.cloudinary.com/dyq0ij1yk/image/upload/f_auto,q_auto:eco,w_auto,dpr_auto,c_limit/v1774167878/pnuioefpb2ofdczcekpq.avif"
@@ -100,7 +100,7 @@ export default function About() {
                 className="object-cover rounded-lg"
               />
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
