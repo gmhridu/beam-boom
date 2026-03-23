96ff94a0a5cb32c88d533aef5b1651b3741a4e3c
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2026-03-24 00:50:53 +0600

    fix: navigation data

--- src/lib/navigation-data.ts
+++ src/lib/navigation-data.ts
@@ -85,12 +85,13 @@ export const navigationData = [
     ],
   },
   { name: "PRICING", href: "/pricing" as Href },
-  {
-    name: "MORE INFO",
-    children: [
-      { name: "Special Offers", href: "/special-offers" as Href },
-      // { name: "Testimonials", href: "/testimonials" as Href },
-    ],
-  },
+  { name: "Special Offers", href: "/special-offers" as Href },
+  // {
+  //   name: "MORE INFO",
+  //   children: [
+  //     { name: "Special Offers", href: "/special-offers" as Href },
+  //     // { name: "Testimonials", href: "/testimonials" as Href },
+  //   ],
+  // },
   { name: "CONTACT US", href: "/contact-us" as Href },
 ] as const satisfies readonly NavigationItem[];
